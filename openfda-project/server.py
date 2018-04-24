b7c9b877f1f2d3de3476f69812eac14c54989154
fj.lara.2017 <franje99@hotmail.es>
2018-04-24 16:59:31 +0200

    update

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -43,7 +43,8 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             conn = http.client.HTTPSConnection("api.fda.gov")
             params = self.path.split("?")[1]
             company = params.split("&")[0].split("=")[1]
-            url = "/drug/label.json?search=brand_name:" + company
+            limit = params.split("&")[1].split("=")[1]
+            url = "/drug/label.json?search=brand_name:" + company + "&" + "limit=" + limit
             print(url)
             conn.request("GET", url, None, headers)
             r1 = conn.getresponse()
